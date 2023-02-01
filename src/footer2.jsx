6e6933af30dae2fea08010a6b66f6436ae8cf23d
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2023-02-01 02:38:16 +0100

    testing footer responsiveness

--- src/footer2.jsx
+++ src/footer2.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 export const Footer2 = () => {
   return (
-    <footer className="container grid grid-cols-1 md:grid-cols-5">
-      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:order-3">
+    <footer className="container grid grid-cols-1 md:grid-cols-5 md:grid-rows-3">
+      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:col-start-3 md:row-span-3">
         <p className="uppercase text-red-500">Logo</p>
       </div>
-      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:order-5">
+      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:col-start-5 md:row-start-2">
         <p className="uppercase text-red-500">Bulle chat</p>
       </div>
-      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:order-1">
+      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:col-start-1 md:row-start-2">
         <p className="uppercase text-red-500">Reseaux sociaux</p>
       </div>
-      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:order-2">
+      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:row-start-3 md:col-start-2">
         <p className="uppercase text-red-500">RGPD1</p>
       </div>
-      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:order-4">
+      <div className="border border-purple-400 rounded-2xl py-1 px-5 md:col-start-4 md:row-start-3">
         <p className="uppercase text-red-500">RGPD2</p>
       </div>
     </footer>
