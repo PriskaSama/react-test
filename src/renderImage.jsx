08320285dd951dce874aea5b5e0aa025a59741d8
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2023-02-01 02:37:56 +0100

    testing responsive img with next, created img component

---
+++ src/renderImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next";
+
+// TODO: Une div contenant le composant IMAGE de Next. Puis ajouter une div contenant la div précendente avec une autre image + une div avec du texte. Checker si le rem du texte permet de tout afficher proportionnellement.
+
+export const RenderImage = () => {
+  return (
+    <div>
+      <Image />
+    </div>
+  );
+};
